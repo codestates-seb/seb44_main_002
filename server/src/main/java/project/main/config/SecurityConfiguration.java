23627b5006f40032416ea8c7decda46cad4eda33
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2023-06-29 18:07:34 +0900

    init : 초기 프로젝트 설정

---
+++ server/src/main/java/project/main/config/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package project.main.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfiguration {
+}
---
+++ server/src/main/java/project/main/dto/MultiValueDto.java
@@ -0,0 +1,4 @@
+package project.main.dto;
+
+public class MultiValueDto {
+}
