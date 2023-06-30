db00af986e70fbeb315c0b25db32f5c51ecafdcc
crowcrow07 <skathd3402@gmail.com>
2023-06-30 14:47:39 +0900

    Init : 라우팅 작업, 파일명 변경, 404페이지

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { store } from './redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
