7b9971188e2e9d677814948fafeb75000a4b0411
jkroh1995 <shworud29@gmail.com>
2023-06-30 14:49:52 +0900

    feat : 예외 처리 기능 구현

---
+++ src/main/java/project/main/exception/BusinessLogicException.java
@@ -0,0 +1,14 @@
+package project.main.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException{
+
+    @Getter
+    private final ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode){
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
---
+++ src/main/java/project/main/exception/response/ErrorResponse.java
@@ -0,0 +1,20 @@
+package project.main.exception.response;
+
+import lombok.Getter;
+import project.main.exception.ExceptionCode;
+
+@Getter
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+
+    private ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode){
+        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
+    }
+}
