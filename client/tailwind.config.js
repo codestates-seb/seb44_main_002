5737fbd41e7a0b7936767eb0e11984ba42a39a7f
crowcrow07 <skathd3402@gmail.com>
2023-06-30 15:42:08 +0900

    fixed : tailwind 오류

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    extend: {},
   },
   plugins: [],
 };
