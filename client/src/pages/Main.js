35fcffb10390ee99bd7bc6f267f6c231ca3efa63
crowcrow07 <skathd3402@gmail.com>
2023-07-01 09:34:43 +0900

    feat : mainPage 기본틀 설정

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,1 +1,9 @@
-export default function Main() {}
+export default function Main() {
+  return (
+    <main className="bg-gradient-to-r from-gradi-to to-gradi-from w-screen h-screen">
+      <section>slider</section>
+      <section>ranking</section>
+      <section>community</section>
+    </main>
+  );
+}
