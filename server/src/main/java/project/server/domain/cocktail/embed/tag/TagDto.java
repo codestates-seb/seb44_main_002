498ac15917e144e51d15f25e05ebf4cf77fbbd63
jkroh1995 <shworud29@gmail.com>
2023-07-02 16:45:11 +0900

    feat : 칵테일 등록 기능 구현 - Embed 클래스 생성 : Recipe, Tags, Rating - Embed 클래스 Dto 구현

---
+++ server/src/main/java/project/server/domain/cocktail/embed/tag/TagDto.java
@@ -0,0 +1,23 @@
+package project.server.domain.cocktail.embed.tag;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class TagDto {
+
+    @Getter
+    @Setter
+    public static class Post{
+        String tag;
+    }
+
+    @Getter
+    @Setter
+    public static class Response{
+        String tag;
+
+        public Response(String tag) {
+            this.tag = tag;
+        }
+    }
+}
--- server/src/main/java/project/server/domain/cocktail/repository/CocktailRepository.java
+++ server/src/main/java/project/server/domain/cocktail/repository/CocktailRepository.java
@@ -1,6 +1,7 @@
-package project.server.domain.cocktail;
+package project.server.domain.cocktail.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.server.domain.cocktail.entity.Cocktail;
 
 public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
 }
