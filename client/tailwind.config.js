3967d019216c7e1a9f57750aa66f681d73545aaa
crowcrow07 <skathd3402@gmail.com>
2023-07-03 17:51:36 +0900

    chore : 색깔 등록

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       colors: {
         'gradi-to': '#1A344A',
         'gradi-from': '#4A4676',
+        'gray-100': '#E6E6E6',
+        'gray-200': '#B3B3B3',
+        'gray-300': '#7B7B7B',
+        'pointPurple-100': '#BB40F1',
+        'pointPurple-200': '#BC4096',
       },
     },
   },
