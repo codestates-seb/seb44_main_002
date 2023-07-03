220b13e6f40142f2b63c3fd9026c6a8ee82f80c7
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-03 16:40:09 +0900

    chore: 카드 클릭시 id 따라 라우팅 변경

--- client/src/pages/Category/Category.js
+++ client/src/pages/Category/Category.js
@@ -3,9 +3,16 @@ export default function Category() {
   //category 변경함 루트 변경했습니다.->import app.js
   //reset.css는 어디있나용?
   //
+
+  const isBookmarked = true;
+  const item = {
+    itemid: '99',
+    img: 'images/cocktailSample.jpg',
+    title: '타이틀',
+  };
   return (
     <div className="bg-gradient-to-r from-gradi-to to-gradi-from w-screen h-screen flex items-center justify-center">
-      <Card />
+      <Card item={item} isBookmarked={isBookmarked} />
     </div>
   );
 }
--- client/src/redux/slice/userInfoSlice.js
+++ client/src/redux/slice/userInfoSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 // {
 //     “name” : “kim”,
 //     “profileImageUrl” : “sample image url”
