bac7a49f62d0a412eda208964ffea0544a0a4e1f
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-03 15:11:53 +0900

    feat: 클릭시 디테일페이지로 이동

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,7 +1,9 @@
 import BookmarkButton from '../BookmarkButton/BookmarkButton';
+import { useNavigate } from 'react-router-dom';
 
 //item 칵테일에 대한 정보가 객체형태로 담겨있습니다.
 export default function Card({ item = { title: '제목' }, isBookmarked }) {
+  const navigate = useNavigate();
   // background: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(20210707095408-69BNH.jpg);
   // box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), -4px -4px 16px rgba(0, 0, 0, 0.25);
 
@@ -10,7 +12,10 @@ export default function Card({ item = { title: '제목' }, isBookmarked }) {
   //const itemImg = item.img || 'images/cocktailSample.jpg';
   const itemImg = 'images/cocktailSample.jpg';
   return (
-    <div className=" relative w-[180px] h-[200px] ">
+    <button
+      className=" relative w-[180px] h-[200px] "
+      onClick={() => navigate('/detail/1')}
+    >
       {/* 북마크 */}
       <div className="absolute top-0  right-2">
         <BookmarkButton item={item} isBookmarked={isBookmarked} />
@@ -23,9 +28,10 @@ export default function Card({ item = { title: '제목' }, isBookmarked }) {
           className="w-[180px] h-[200px] rounded-tl-2xl rounded-br-2xl "
         />
       </div>
+      {/* 하단 칵테일이름 */}
       <h3 className="text-[20px] font-bold text-[#B3B3B3]  hover:text-white">
         {title}
       </h3>
-    </div>
+    </button>
   );
 }
