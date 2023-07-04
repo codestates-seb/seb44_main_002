2614bc15a9327970e79407d09d699d5dc48fa172
crowcrow07 <skathd3402@gmail.com>
2023-07-04 10:24:27 +0900

    chore : 슬라이더 샘플사진 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,7 +2,7 @@ import Slider from '../components/Slider/Slider';
 
 export default function Main() {
   return (
-    <main className="bg-gradient-to-r from-gradi-to to-gradi-from h-screen">
+    <main className="bg-gradient-to-r from-gradi-to to-gradi-from">
       <section className="h-screen">
         <Slider />
       </section>
