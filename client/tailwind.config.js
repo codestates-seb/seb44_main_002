4bd77cf5a6c5c75bdc7334760c3723e3e2460815
HChanWoo <cksdn1st@naver.com>
2023-07-05 11:49:41 +0900

    chore: gray 색상 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'gray-100': '#E6E6E6',
         'gray-200': '#B3B3B3',
         'gray-300': '#7B7B7B',
+        'gray-400': '#8F8F8F',
         'pointPurple-100': '#BB40F1',
         'pointPurple-200': '#BC4096',
       },
