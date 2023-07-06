30447055470e289c07f5fd6d6a81e77e21bcf36a
jkroh1995 <shworud29@gmail.com>
2023-07-07 00:13:28 +0900

    feat : reply entity 클래스 생성

--- server/src/main/java/project/server/domain/reply/Reply.java
+++ server/src/main/java/project/server/domain/reply/Reply.java
@@ -3,13 +3,13 @@ package project.server.domain.reply;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import net.bytebuddy.utility.nullability.MaybeNull;
+import project.server.domain.comment.entity.Comment;
+import project.server.domain.user.User;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity(name = "entities")
+@Entity(name = "replies")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,4 +18,15 @@ public class Reply {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long replyId;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Comment comment;
+
+    private String content;
+
+    @Embedded
+    private TaggedUserInfo taggedUserInfo;
 }
