2763cf491636edaade2437e122f50015e7b5410c
HChanWoo <cksdn1st@naver.com>
2023-07-08 17:44:50 +0900

    feat: 유저페이지 구독한 사람 1차 완성

---
+++ client/src/pages/UserPage/Subscribe.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+SwiperCore.use([Navigation, Pagination]);
+
+import tw from 'tailwind-styled-components';
+import './Swiper.css';
+
+export default function Subscribe({ userInfo }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  let slidesPerView = 5; // 창 너비에 따라 slidesPerView 값 설정
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (windowWidth <= 850) {
+    slidesPerView = 3;
+  } else if (windowWidth <= 1200) {
+    slidesPerView = 4;
+  }
+
+  const UserList = () => {
+    return (
+      <div id="userpage">
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={slidesPerView}
+          navigation
+          pagination={{ clickable: true }}
+          className="mySwiper"
+        >
+          {userInfo.subscribe.map((user) => {
+            return (
+              <SwiperSlide key={user.userId}>
+                <div
+                  key={user.userId}
+                  className="border-dotted border-[1px] rounded"
+                >
+                  <img
+                    className="object-cover"
+                    src={user.profileImageUrl}
+                    alt="user profile"
+                  />
+                  <p>{user.name}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+      // <div className="flex">
+      //   {userInfo.subscribe.map((user) => {
+      //     return (
+      //       <div
+      //         key={user.userId}
+      //         className="mr-4 py-3 px-8 border-dotted border-[1px] rounded"
+      //       >
+      //         <img
+      //           className="w-20 object-cover"
+      //           src={user.profileImageUrl}
+      //           alt="user profile"
+      //         />
+      //         <p className="mt-4">{user.name}</p>
+      //       </div>
+      //     );
+      //   })}
+      // </div>
+    );
+  };
+  return (
+    <Container>
+      <p className="text-2xl">{userInfo.name + '님이  구독한 사람.'}</p>
+      <UserList />
+    </Container>
+  );
+}
+const Container = tw.div`
+text-white
+mt-32
+mx-12
+`;
