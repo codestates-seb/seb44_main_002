4f14a1c99c372cd526d20351a528742372cc29a7
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-13 23:54:43 +0900

    feat: 댓글인지 대댓글인지 여부로 패치요청 분기

--- client/src/pages/Comment/CommentPage.js
+++ client/src/pages/Comment/CommentPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import tw from 'tailwind-styled-components';
+import CommentValid from '../../components/Validation/CommentValidation';
 // [
 //   {
 //       “commentId” : 1,
@@ -26,16 +27,85 @@ import tw from 'tailwind-styled-components';
 //   }
 // ],
 export default function CommentPage() {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const { state } = useLocation();
   const [isCommented, setIsCommented] = useState(state[0]);
   const commentdata = state[1];
   const [isValid, setIsValid] = useState(true);
   const [comment, setComment] = useState(commentdata.content);
+  const [errorMsg, setErrorMsg] = useState(null);
   const navigate = useNavigate();
   console.log(state);
   const handleSubmit = (e) => {
     e.preventDefault();
     CommentValid(comment, setIsValid);
+    //유효성검사가 통과되었다면
+    if (isValid) {
+      //댓글 수정이라면
+      if (isCommented) {
+        fetch(`${BASE_URL}comments/${commentdata.commentId}`, {
+          method: 'PATCH',
+          headers: {
+            //'ngrok-skip-browser-warning': 'true',
+            'Content-Type': 'application/json', // json fetch시
+          },
+          body: JSON.stringify({ content: comment }),
+        })
+          .then((data) => {
+            if (data.status === 200) {
+              // 응답이 성공적인 경우
+              console.log('요청이 성공했습니다.');
+              // console.log(data);
+              setErrorMsg(null);
+              navigate('/');
+              alert('수정했습니다!');
+            } else {
+              // 응답이 실패한 경우
+              console.log('요청이 실패했습니다.');
+            }
+          })
+          .catch((error) => {
+            console.log('에러', error);
+            navigate('/error');
+          });
+
+        //대댓글 수정이라면
+      } else {
+        fetch(`${BASE_URL}replies/${commentdata.replyId}`, {
+          method: 'PATCH',
+          headers: {
+            //'ngrok-skip-browser-warning': 'true',
+            'Content-Type': 'application/json', // json fetch시
+          },
+          body: JSON.stringify({
+            userId: commentdata.userId,
+            taggedUserId: commentdata.taggedUserId,
+            taggedUserName: commentdata.taggedUserName,
+            content: commentdata.content,
+          }),
+        })
+          .then((data) => {
+            if (data.status === 200) {
+              // 응답이 성공적인 경우
+              console.log('요청이 성공했습니다.');
+              // console.log(data);
+              setErrorMsg(null);
+              navigate('/');
+              alert('수정했습니다!');
+            } else {
+              // 응답이 실패한 경우
+              console.log('요청이 실패했습니다.');
+            }
+          })
+          .catch((error) => {
+            console.log('에러', error);
+            navigate('/error');
+          });
+      }
+    } else {
+      console.log('유효성검사에 걸렸습니다.');
+      setErrorMsg('1~200사이 글자로 입력해주세요');
+    }
   };
 
   return (
@@ -70,6 +140,7 @@ export default function CommentPage() {
             <p className="text-red-500">1~200자 범위내로 작성해주세요</p>
           )}
         </div>
+        {errorMsg && <p className="text-error ">{errorMsg}</p>}
         <InputButton onClick={handleSubmit}>전송하기</InputButton>
       </section>
     </div>
