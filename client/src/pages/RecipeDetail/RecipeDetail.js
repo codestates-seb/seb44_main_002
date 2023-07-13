7db1e65a3e0fe71369d0dca33e2076bf9db04769
HChanWoo <cksdn1st@naver.com>
2023-07-13 22:38:31 +0900

    fix: 더미데이터 작업

--- client/src/pages/RecipeDetail/RecipeDetail.js
+++ client/src/pages/RecipeDetail/RecipeDetail.js
@@ -128,7 +128,11 @@ export default function RecipeDetail() {
             getTime={getTime}
           />
           <Process cocktailDetail={cocktail} />
-          <Community cocktailDetail={cocktail} userInfo={userInfo} />
+          <Community
+            cocktailDetail={cocktail}
+            userInfo={userInfo}
+            getTime={getTime}
+          />
           <Recommend cocktailDetail={cocktail.recommend} userInfo={userInfo} />
         </Container>
       </Background>
@@ -171,8 +175,8 @@ const cocktailDetail = {
   imageUrl: 'sample image url',
   liquor: '럼',
   viewCount: 1,
-  createdAt: '2000-00-00',
-  modifiedAt: '2000-00-00',
+  createdAt: '2023-07-02T01:01:01',
+  modifiedAt: '2023-07-02T01:01:01',
   ingredients: [
     {
       ingredient: 'Light rum',
@@ -225,15 +229,16 @@ const cocktailDetail = {
     {
       commentId: 1,
       userId: 2,
-      name: 'kim',
+      userName: 'kim',
       content:
         '깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!깔끔하고 맛있네요!',
-      date: '2023-02-16',
+      createdAt: '2023-07-02T01:01:01',
+      modifiedAt: '2023-07-02T01:01:01',
       replies: [
         {
           replyId: 1,
           userId: 3,
-          name: 'chan',
+          userName: 'chan',
           content: '저도 그렇게 생각합니다!',
           taggedUserInfo: [
             {
@@ -241,35 +246,47 @@ const cocktailDetail = {
               taggedUserName: 'kimchi',
             },
           ],
-          date: '2023-02-16',
+          createdAt: '2023-07-02T01:01:01',
+          modifiedAt: '2023-07-02T01:01:01',
         },
       ],
     },
     {
       commentId: 2,
       userId: 3,
-      name: 'chan',
+      userName: 'chan',
       content:
         '그놈은 멋있었다...백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.백엔드는 멋있었다.',
-      date: '2023-02-16',
+      createdAt: '2023-07-02T01:01:01',
+      modifiedAt: '2023-07-02T01:01:01',
       replies: [
         {
           replyId: 2,
           userId: 4,
-          name: 'jae',
+          userName: 'jae',
           content: '백엔드는 멋있다.',
-          taggedUserId: 3,
-          taggedUserName: 'chan',
-          date: '2023-02-16',
+          taggedUserInfo: [
+            {
+              taggedUserId: 3,
+              taggedUserName: 'chan',
+            },
+          ],
+          createdAt: '2023-07-02T01:01:01',
+          modifiedAt: '2023-07-02T01:01:01',
         },
         {
           userId: 3,
-          name: 'euni',
+          userName: 'euni',
           content:
             '이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면 이제 아셨습니까. 휴면',
-          taggedUserId: 4,
-          taggedUserName: 'jae',
-          date: '2023-02-16',
+          taggedUserInfo: [
+            {
+              taggedUserId: 4,
+              taggedUserName: 'jae',
+            },
+          ],
+          createdAt: '2023-07-02T01:01:01',
+          modifiedAt: '2023-07-02T01:01:01',
         },
       ],
     },
@@ -295,6 +312,6 @@ const cocktailDetail = {
       isBookmarked: false,
     },
   ],
-  bookmarked: false,
+  isBookmarked: false,
   adminWritten: false,
 };
