d2d0ad2a4b7de86e09206839568af5458611daac
HChanWoo <cksdn1st@naver.com>
2023-07-13 18:12:27 +0900

    feat: 상세페이지에 사용할 api 구현

--- client/src/pages/RecipeDetail/RecipeApi.js
+++ client/src/pages/RecipeDetail/RecipeApi.js
@@ -1,10 +1,130 @@
 const API_BASE = process.env.REACT_APP_BASE_URL;
 
 export default {
+  // 칵테일 정보 가져오기
   async getCocktailData(cocktailId) {
     try {
       const response = await fetch(`${API_BASE}cocktails/${cocktailId}`, {
         method: 'GET',
+      });
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 별점 등록, 수정
+  async modifyRate(cocktailId, score, accessToken) {
+    try {
+      const response = await fetch(
+        `${API_BASE}cocktails/${cocktailId}/rate?value=${score}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: accessToken,
+          },
+        }
+      );
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 댓글 등록
+  async PostComments(cocktailId, commentInfo, accessToken) {
+    try {
+      const response = await fetch(`${API_BASE}comments/${cocktailId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+        body: JSON.stringify(commentInfo),
+      });
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 답변 등록
+  async PostReplys(cocktailId, replyInfo, accessToken) {
+    try {
+      const response = await fetch(`${API_BASE}replies/${cocktailId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+        body: JSON.stringify(replyInfo),
+      });
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 댓글 삭제
+  async deleteComments(commentId, accessToken) {
+    try {
+      const response = await fetch(`${API_BASE}comments/${commentId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+      });
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 칵테일 레시피 삭제
+  async deleteCocktails(cocktailId, accessToken) {
+    try {
+      const response = await fetch(`${API_BASE}cocktails/${cocktailId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+      });
+      if (response.ok) {
+        return response;
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  // 답변 삭제
+  async deleteReplies(replyId, accessToken) {
+    try {
+      const response = await fetch(`${API_BASE}replies/${replyId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
       });
       if (response.ok) {
         return response;
