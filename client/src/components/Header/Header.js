1aa8d80a2d0f93b2f009a2a9be32005b48bb9b65
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-16 11:02:20 +0900

    feat: 모달 라이브러리 적용

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../redux/slice/isLoginSlice';
 import { userinfoLoginOut } from '../../redux/slice/userInfoSlice';
+import Swal from 'sweetalert2';
 
 import HeaderModal from './HeaderModal';
 import Hamburger from './Hamburger';
@@ -16,6 +17,8 @@ export default function Header() {
 
   const navigate = useNavigate();
   const location = useLocation();
+
+  const Swal = require('sweetalert2');
 
   // 리덕스 툴킷
   const isLogin = useSelector((state) => state.isLogin.isLogin);
@@ -137,7 +140,28 @@ export default function Header() {
             <div className="mx-2">
               <LogoutIcon
                 onClick={() =>
-                  window.confirm('로그아웃하시겠습니까?') ? handleLogOut() : ''
+                  // window.confirm('로그아웃하시겠습니까?') ? handleLogOut() : ''
+
+                  Swal.fire({
+                    title: '정말로 로그아웃하시겠습니까?',
+                    text: '정말로 로그아웃하시겠습니까?',
+                    icon: 'warning',
+
+                    showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+                    confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+                    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+                    confirmButtonText: '로그아웃하기', // confirm 버튼 텍스트 지정
+                    cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+                    reverseButtons: true, // 버튼 순서 거꾸로
+                  }).then((result) => {
+                    // 만약 Promise리턴을 받으면,
+                    if (result.isConfirmed) {
+                      // 만약 모달창에서 confirm 버튼을 눌렀다면
+
+                      handleLogOut();
+                    }
+                  })
                 }
                 fontSize="large"
                 style={{ cursor: 'pointer' }}
