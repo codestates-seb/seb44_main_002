d08d722e45f58b7237cfca72551f6e7d0fe8a333
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-16 14:55:47 +0900

    fix: 모달 에러 수정

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -49,10 +49,10 @@ export default function Header() {
     </div>
   );
   const handleLogOut = () => {
-    // console.log('동작');
+    console.log('동작');
     localStorage.clear();
-    dispatch(userinfoLoginOut());
     dispatch(logout());
+    dispatch(userinfoLoginOut());
     navigate('/');
   };
 
@@ -140,25 +140,24 @@ export default function Header() {
             <div className="mx-2">
               <LogoutIcon
                 onClick={() =>
-                  // window.confirm('로그아웃하시겠습니까?') ? handleLogOut() : ''
+                  // window.confirm('로그아웃하시겠습니까?')
+                  //   ? handleLogOut()
+                  //   : ''
 
                   Swal.fire({
                     title: '정말로 로그아웃하시겠습니까?',
                     text: '정말로 로그아웃하시겠습니까?',
                     icon: 'warning',
-
                     showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
                     confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
                     cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
                     confirmButtonText: '로그아웃하기', // confirm 버튼 텍스트 지정
                     cancelButtonText: '취소', // cancel 버튼 텍스트 지정
-
                     reverseButtons: true, // 버튼 순서 거꾸로
                   }).then((result) => {
                     // 만약 Promise리턴을 받으면,
                     if (result.isConfirmed) {
                       // 만약 모달창에서 confirm 버튼을 눌렀다면
-
                       handleLogOut();
                     }
                   })
--- client/src/components/Header/HeaderModal.js
+++ client/src/components/Header/HeaderModal.js
@@ -114,6 +114,7 @@ export default function HeaderModal() {
             handleUserInfo(data.headers.get('UserId'));
             // 전역상태관리 로그인으로 변경
             dispatch(login(() => login()));
+            handleClose();
             navigate('/');
           } else {
             if (data.status === 401) {
@@ -124,6 +125,7 @@ export default function HeaderModal() {
         })
         .catch((err) => {
           console.log(err);
+          handleClose();
           navigate('/error');
         });
     } else {
