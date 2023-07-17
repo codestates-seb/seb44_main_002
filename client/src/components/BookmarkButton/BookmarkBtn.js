47cad66e3079c4bb4c694e02f5a078d716d08ef4
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-17 22:09:44 +0900

    fix: 북마크 수정

--- client/src/components/BookmarkButton/BookmarkBtn.js
+++ client/src/components/BookmarkButton/BookmarkBtn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import tw from 'tailwind-styled-components';
 // 사용법
@@ -14,18 +14,21 @@ import tw from 'tailwind-styled-components';
 }
 export default function BookmarkBtn({
   onClick,
-  isBookmarked,
+  bookmarked,
+  setbookmarked,
   size = 'w-[36px] h-[60px]',
   absolute,
   top,
   right,
 }) {
   const isLogin = useSelector((state) => state.isLogin.isLogin);
+
   return (
     <Button
       onClick={(e) => {
         if (isLogin) {
           e.stopPropagation();
+          setbookmarked(!bookmarked);
           onClick();
         } else {
           alert('로그인해야 가능합니다');
@@ -35,7 +38,7 @@ export default function BookmarkBtn({
       top={top}
       right={right}
     >
-      {isBookmarked ? (
+      {bookmarked ? (
         <Img
           src="/images/bookmark/bookmarkOn.png"
           alt="활성화된 북마크"
