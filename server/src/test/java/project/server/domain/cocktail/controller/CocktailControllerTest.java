9ebf01101325fd860bd45c9d6a4fbbe3ad2c6d85
jkroh1995 <shworud29@gmail.com>
2023-07-18 16:32:31 +0900

    test : 칵테일 등록, 조회, 별점 등록 테스트 작성

--- server/src/test/java/project/server/domain/cocktail/controller/CocktailControllerTest.java
+++ server/src/test/java/project/server/domain/cocktail/controller/CocktailControllerTest.java
@@ -1,6 +1,7 @@
 package project.server.domain.cocktail.controller;
 
 import com.google.gson.Gson;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +12,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import project.server.domain.bookmark.service.BookmarkService;
 import project.server.domain.cocktail.dto.CocktailDto;
 import project.server.domain.cocktail.embed.ingredient.IngredientDto;
 import project.server.domain.cocktail.embed.recipe.RecipeDto;
@@ -21,7 +23,9 @@ import project.server.domain.user.UserService;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,8 +42,11 @@ class CocktailControllerTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BookmarkService bookmarkService;
+
     @BeforeAll
-    void createUser(){
+    void createUser() {
         UserDto.Post post = new UserDto.Post();
         post.setName("1");
         post.setPassword("1234");
@@ -53,8 +60,10 @@ class CocktailControllerTest {
     @DisplayName("칵테일 등록 컨트롤러 테스트")
     @WithUserDetails(value = "11@11.com", userDetailsServiceBeanName = "detailsService")
     void 칵테일_등록_테스트() throws Exception {
-        RecipeDto.Post recipe = new RecipeDto.Post();
-        recipe.setProcess("1");
+        RecipeDto.Post process1 = new RecipeDto.Post();
+        process1.setProcess("1");
+        RecipeDto.Post process2 = new RecipeDto.Post();
+        process2.setProcess("2");
         TagDto.Post tag = new TagDto.Post();
         tag.setTag("sweet");
         IngredientDto.Post ingredient = new IngredientDto.Post();
@@ -63,7 +72,7 @@ class CocktailControllerTest {
         CocktailDto.Post post = new CocktailDto.Post();
         post.setName("test");
         post.setDegree("frequency_high");
-        post.setRecipe(List.of(recipe));
+        post.setRecipe(List.of(process1, process2));
         post.setFlavor(List.of(tag));
         post.setLiquor("rum");
         post.setIngredients(List.of(ingredient));
@@ -71,14 +80,72 @@ class CocktailControllerTest {
 
         String content = gson.toJson(post);
 
-        ResultActions actions =
-                mockMvc.perform(
-                        post("/cocktails")
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(content)
-                );
+        mockMvc.perform(
+                post("/cocktails")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        ).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("test"))
+                .andExpect(jsonPath("$.userName").value("1"))
+                .andExpect(jsonPath("$.recipe[*].process").value(Matchers.contains("1", "2")))
+                .andExpect(jsonPath("$.ingredients[0].ingredient").value("우유"))
+                .andExpect(jsonPath("$.liquor").value("럼"))
+                .andExpect(jsonPath("$.tags[*].tag").value(Matchers.contains("# 단맛", "# 도수 높음")))
+                .andExpect(jsonPath("$.imageUrl").value("sample image url"));
 
-        actions.andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 로그인_하지_않은_사용자_칵테일_조회_테스트() throws Exception {
+        mockMvc.perform(
+                        get("/cocktails/1")
+                                .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("test1"))
+                .andExpect(jsonPath("$.bookmarked").value("false"));
+    }
+
+    @Test
+    @WithUserDetails(value = "11@11.com", userDetailsServiceBeanName = "detailsService")
+    void 로그인한_사용자의_북마크_이후_칵테일_조회_테스트() throws Exception {
+        bookmarkService.createBookmark("11@11.com", 1L);
+
+        mockMvc.perform(
+                        get("/cocktails/1")
+                                .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("test1"))
+                .andExpect(jsonPath("$.bookmarked").value("true"));
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 칵테일_검색_테스트(){
+
+    }
+
+    // app configure 삭제하면 새롭게 칵테일 등록 로직 작성 후 테스트 고쳐야함.
+    @Test
+    @WithAnonymousUser
+    void 랜덤_칵테일_조회_테스트() throws Exception {
+        mockMvc.perform(
+                        get("/cocktails/random")
+                                .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk());
+//                .andExpect(jsonPath("$.name").value("test1"))
+//                .andExpect(jsonPath("$.bookmarked").value("false"));
+    }
+
+    @Test
+    @WithUserDetails(value = "11@11.com", userDetailsServiceBeanName = "detailsService")
+    void 칵테일_별점_등록_테스트() throws Exception {
+        mockMvc.perform(
+                post("/cocktails/1/rate?value=3")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.rating").value(3.0));
     }
 }
