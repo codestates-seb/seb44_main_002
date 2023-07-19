1bc855e88b8021b7705c3566b46e2830f8adb578
crowcrow07 <skathd3402@gmail.com>
2023-07-19 15:09:42 +0900

    fixed : ingredients -> baseIngredients

--- client/src/components/Validation/CocktailFormValidation.js
+++ client/src/components/Validation/CocktailFormValidation.js
@@ -7,7 +7,7 @@ export default function useCocktailFormValid(form) {
   const nameIsValid = Boolean(form.name) && nameRegex.test(form.name);
   const imageIsValid = form.imageUrl !== '';
   const liquorIsValid = form.liquor !== '';
-  const ingredientsIsValid = form.ingredients.length !== 0;
+  const baseIngredientsIsValid = form.baseIngredients.length !== 0;
   const recipeIsValid = recipeRegex.length === 0;
   const degreeTagIsValid = form.degree.length !== 0;
   const flavorTagIsValid = form.flavor.length !== 0;
@@ -16,7 +16,7 @@ export default function useCocktailFormValid(form) {
     name: nameIsValid,
     imageUrl: imageIsValid,
     liquor: liquorIsValid,
-    ingredients: ingredientsIsValid,
+    baseIngredients: baseIngredientsIsValid,
     recipe: recipeIsValid,
     degree: degreeTagIsValid,
     flavor: flavorTagIsValid,
