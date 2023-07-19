ae089feb877090676ca925ed2e5ea7126b0b6347
HChanWoo <cksdn1st@naver.com>
2023-07-19 16:04:54 +0900

    feat: 5개 이상 조건부 무한 루프

--- client/src/pages/UserPage/Subscribe.js
+++ client/src/pages/UserPage/Subscribe.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import UserPageApi from './UserPageApi';
 
+import tw from 'tailwind-styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import './Swiper.css';
+
 SwiperCore.use([Navigation, Pagination]);
 
-import tw from 'tailwind-styled-components';
-import './Swiper.css';
-import { Link } from 'react-router-dom';
-
 export default function Subscribe({ userInfo }) {
+  const [follows, setFollows] = useState([]);
+
   const cancleFollow = async (id) => {
     try {
       const response = await UserPageApi.cancelfollow(id);
@@ -20,10 +22,20 @@ export default function Subscribe({ userInfo }) {
     }
   };
 
-  const deleteSubscriber = (event, id) => {
+  const deleteSubscriber = (event, id, name) => {
     event.stopPropagation();
-    cancleFollow(id);
+    if (window.confirm(`${name} 님의 구독을 취소하시겠습니까?`)) {
+      cancleFollow(id);
+    }
   };
+
+  useEffect(() => {
+    // 슬라이더 무한루프 구현(5개 이상만 가능합니다.)
+    if (userInfo.follows.length >= 5) {
+      userInfo.follows = [...userInfo.follows, ...userInfo.follows];
+    }
+    setFollows(userInfo.follows);
+  }, [userInfo]);
 
   const UserList = () => {
     return (
@@ -34,40 +46,44 @@ export default function Subscribe({ userInfo }) {
           </p>
         ) : (
           <Swiper
-            spaceBetween={10} //좌우측
-            slidesPerView={2} //
+            spaceBetween={30} //좌우측
+            slidesPerView={2}
             slidesPerGroup={2} // 한 화면에 나오는 슬라이드 넘어가는 수
-            loop={true}
+            loop={userInfo.follows.length >= 5 ? true : false}
             navigation={true}
-            pagination={{ clickable: true }}
             className="mySwiper"
             breakpoints={{
-              500: { slidesPerView: 3, slidesPerGroup: 2 },
-              850: { slidesPerView: 4, slidesPerGroup: 2 },
+              550: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 50 },
+              950: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 50 },
+              1200: { slidesPerView: 5, slidesPerGroup: 5, spaceBetween: 55 },
             }}
           >
-            {userInfo.follows.map((user) => {
+            {follows.map((user, idx) => {
               return (
-                <SwiperSlide key={user.followingUserId}>
-                  <Link to={`/userpage/${user.followingUserId}`}>
-                    <div>
+                <SwiperSlide key={user.followingUserId + idx}>
+                  <div>
+                    <Link to={`/userpage/${user.followingUserId}`}>
                       <img
                         src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
                         alt="user profile"
                       />
-                      <p id="name">{user.followingUserName}</p>
-                      <CloseP
-                        id="close"
-                        role="presentation"
-                        onClick={(event) =>
-                          deleteSubscriber(event, user.followingUserId)
-                        }
-                        onKeyDown={() => {}}
-                      >
-                        x
-                      </CloseP>
-                    </div>
-                  </Link>
+                      <UserName id="name">{user.followingUserName}</UserName>
+                    </Link>
+                    <CloseP
+                      id="close"
+                      role="presentation"
+                      onClick={(event) =>
+                        deleteSubscriber(
+                          event,
+                          user.followingUserId,
+                          user.followingUserName
+                        )
+                      }
+                      onKeyDown={() => {}}
+                    >
+                      x
+                    </CloseP>
+                  </div>
                 </SwiperSlide>
               );
             })}
@@ -91,9 +107,14 @@ mx-12
 const Title = tw.p`
 text-2xl
 `;
+const UserName = tw.p`
+text-center
+truncate
+`;
 const CloseP = tw.p`
 px-2.5
 pb-2.5
+z-10
 cursor-pointer
 hover:text-pointPurple-100
 `;
