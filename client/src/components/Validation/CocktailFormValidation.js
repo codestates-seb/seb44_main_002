68e3fdeea0556e0a6e9a5f12e9628b7cdcc52b4e
crowcrow07 <skathd3402@gmail.com>
2023-07-19 23:18:28 +0900

    fixed : 기본 재료 삭제 -> 추가재료를 기본재료로 변경

--- client/src/components/Validation/CocktailFormValidation.js
+++ client/src/components/Validation/CocktailFormValidation.js
@@ -7,7 +7,7 @@ export default function useCocktailFormValid(form) {
   const nameIsValid = Boolean(form.name) && nameRegex.test(form.name);
   const imageIsValid = form.imageUrl !== '';
   const liquorIsValid = form.liquor !== '';
-  const baseIngredientsIsValid = form.baseIngredients.length !== 0;
+  const ingredientsIsValid = form.ingredients.length !== 0;
   const recipeIsValid = recipeRegex.length === 0;
   const degreeTagIsValid = form.degree.length !== 0;
   const flavorTagIsValid = form.flavor.length !== 0;
@@ -16,7 +16,7 @@ export default function useCocktailFormValid(form) {
     name: nameIsValid,
     imageUrl: imageIsValid,
     liquor: liquorIsValid,
-    baseIngredients: baseIngredientsIsValid,
+    ingredients: ingredientsIsValid,
     recipe: recipeIsValid,
     degree: degreeTagIsValid,
     flavor: flavorTagIsValid,
