ac6a756fd4b1b947df72a9bbf79ad7294fd3f03a
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-07-20 15:49:37 +0900

    chore: 기타

--- client/src/api/CocktailFormApi.js
+++ client/src/api/CocktailFormApi.js
@@ -1,15 +1,16 @@
 import api from './api';
 export const PostCocktailForm = async (form) => {
-  const cocktailData = await api
-    .fetchWithInterceptor(`${process.env.REACT_APP_BASE_URL}cocktails`, {
+  const cocktailData = await fetch(
+    `${process.env.REACT_APP_BASE_URL}cocktails`,
+    {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: localStorage.getItem('accessToken'),
       },
       body: JSON.stringify(form),
-    })
-    .then((res) => res.json());
+    }
+  ).then((res) => res.json());
   return cocktailData;
 };
 
