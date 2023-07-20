1c18ff5fee2b0f5e71a8748ded3e5cb4209c7bd5
HChanWoo <cksdn1st@naver.com>
2023-07-20 16:05:09 +0900

    chore: 카테고리 필터와 태그 반응형 스타일 수정

--- client/src/pages/Category/TagFrequencyButton.js
+++ client/src/pages/Category/TagFrequencyButton.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HoverButton from '../../common/Buttons/HoverButton';
 
 export default function TagFrequencyButton({
@@ -21,6 +21,32 @@ export default function TagFrequencyButton({
       onClick();
     }
   };
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    console.log(width);
+    if (width <= 700) {
+      setButtonStyle({
+        size: 'w-[90px] h-[30px]',
+        fontSize: 'text-[0.8rem]',
+        radius: 'rounded-[30px]',
+      });
+    } else {
+      setButtonStyle({
+        size: 'w-[110px] h-[30px]',
+        fontSize: 'text-[1rem]',
+        radius: 'rounded-[30px]',
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <HoverButton
