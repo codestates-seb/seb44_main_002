cb8193730c5982e09998e1c9dc77fb9933862a7d
jkroh1995 <shworud29@gmail.com>
2023-07-22 01:50:30 +0900

    feat : RecommendController 및 RecommendService 로그 추가

--- server/src/main/java/project/server/domain/recommend/service/RecommendService.java
+++ server/src/main/java/project/server/domain/recommend/service/RecommendService.java
@@ -1,5 +1,6 @@
 package project.server.domain.recommend.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import project.server.domain.recommend.dto.RecommendDto;
 import project.server.domain.recommend.entity.Recommend;
@@ -9,6 +10,7 @@ import project.server.domain.user.service.UserService;
 import java.util.List;
 
 @Service
+@Slf4j
 public class RecommendService {
 
     private final RecommendReadService recommendReadService;
@@ -22,6 +24,7 @@ public class RecommendService {
 
     public RecommendDto.UnsignedResponse readRecommendCocktailsForUnsignedUser() {
         List<Recommend> bestCocktails =  recommendReadService.readBestCocktails();
+        log.info("# 비로그인 사용자용 추천 칵테일 조회 성공");
         return new RecommendDto.UnsignedResponse(bestCocktails);
     }
 
@@ -29,6 +32,7 @@ public class RecommendService {
         User user = userService.findUserByEmail(email);
         List<Recommend> bestCocktails = recommendReadService.readBestCocktails();
         List<Recommend> recommends = recommendReadService.readRecommendCocktails(user);
+        log.info("# userId : {}, userAge : {}, userGender : {} 추천 칵테일 조회 성공", user.getUserId(), user.getAge(), user.getGender());
         return new RecommendDto.SignedResponse(bestCocktails, recommends);
     }
 }
