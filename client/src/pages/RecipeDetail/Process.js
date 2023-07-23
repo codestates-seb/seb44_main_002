acb1b18f11247f6c957cba94a19e1791c01286dd
HChanWoo <cksdn1st@naver.com>
2023-07-23 15:09:49 +0900

    feat: 제조과정 복사 기능과 버튼 수정

--- client/src/pages/RecipeDetail/Process.js
+++ client/src/pages/RecipeDetail/Process.js
@@ -6,8 +6,12 @@ export default function Process({ cocktailDetail }) {
   const [hoveredIndex, setHoveredIndex] = useState(-1);
 
   const clickCopy = () => {
+    let formattedRecipe = '';
+    cocktailDetail.recipe.forEach((step, index) => {
+      formattedRecipe += `${index + 1}. ${step.process}\n`;
+    });
     navigator.clipboard
-      .writeText(cocktailDetail.recipe.map((step) => step.process).join(' > '))
+      .writeText(formattedRecipe)
       .then(() => {
         alert('레시피가 클립보드에 복사되었습니다.');
       })
@@ -17,45 +21,47 @@ export default function Process({ cocktailDetail }) {
   };
 
   return (
-    <ProcessContainer>
-      <ProcessHeader>
-        <p>제조과정</p>
-        <ProcessHr />
-      </ProcessHeader>
-      {cocktailDetail.recipe.map((ele, idx) => {
-        return (
-          <ProcessP
-            key={idx}
-            onMouseEnter={() => setHoveredIndex(idx)}
-            onMouseLeave={() => setHoveredIndex(-1)}
-          >
-            {hoveredIndex === idx ? (
-              <HoverdNum>{idx + 1}</HoverdNum>
-            ) : (
-              <ProcessNum>{idx + 1}</ProcessNum>
-            )}
-            <p>{ele.process}</p>
-          </ProcessP>
-        );
-      })}
-      <div className="text-right">
+    <>
+      <ProcessContainer>
+        <ProcessHeader>
+          <p>제조과정</p>
+          <ProcessHr />
+        </ProcessHeader>
+        {cocktailDetail.recipe.map((ele, idx) => {
+          return (
+            <ProcessP
+              key={idx}
+              onMouseEnter={() => setHoveredIndex(idx)}
+              onMouseLeave={() => setHoveredIndex(-1)}
+            >
+              {hoveredIndex === idx ? (
+                <HoverdNum>{idx + 1}</HoverdNum>
+              ) : (
+                <ProcessNum>{idx + 1}</ProcessNum>
+              )}
+              <p>{ele.process}</p>
+            </ProcessP>
+          );
+        })}
+      </ProcessContainer>
+      <div className="text-right mt-4">
         <button
-          className="mt-2 text-right cursor-pointer hover:text-white"
+          className="text-gray-300 cursor-pointer border-[1px] border-gray-300 px-3 py-2 rounded-md hover:text-white hover:border-white"
           onClick={clickCopy}
         >
           제조과정 복사
         </button>
       </div>
-    </ProcessContainer>
+    </>
   );
 }
 const ProcessContainer = tw.div`
 mt-14
 pb-4
-text-[#7b7b7b]
+text-gray-300
 text-xs
 border-b-[1px]
-border-[#7b7b7b]/50
+border-gray-300/50
 `;
 const ProcessHeader = tw.div`
 flex
@@ -65,7 +71,7 @@ const ProcessHr = tw.hr`
 ml-2 
 border-1 
 border-solid 
-border-[#7b7b7b]/50
+border-gray-300/50
 w-[calc(100%-53px)]
 `;
 const ProcessP = tw.div`
@@ -80,7 +86,7 @@ mr-2
 px-1
 py-0.5
 border-[1px] 
-border-[#7b7b7b]/50
+border-gray-300/50
 h-5
 text-[10px]
 `;
