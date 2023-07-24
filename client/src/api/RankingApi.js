b9c2feec0b5ef728eb49835a6dde1753b347b89e
crowcrow07 <skathd3402@gmail.com>
2023-07-24 09:35:42 +0900

    refactor : 주석 제거

--- client/src/api/RankingApi.js
+++ client/src/api/RankingApi.js
@@ -1,7 +1,6 @@
 export const RankingApi = async (isLogin) => {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
   let url = `${BASE_URL}recommend/unsigned`;
-  console.log(url);
   let headers = {
     'Content-Type': 'application/json',
   };
@@ -19,12 +18,9 @@ export const RankingApi = async (isLogin) => {
     headers: headers,
   });
   if (rankingData.status === 401) {
-    console.log('로그아웃해야함.');
     return 401;
   }
   if (rankingData.status === 500) {
-    console.log('재발급받고 엑세스 토큰담겨있음.');
-
     localStorage.setItem(
       'accessToken',
       rankingData.headers.get('Authorization')
