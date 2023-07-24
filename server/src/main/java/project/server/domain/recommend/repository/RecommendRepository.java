f78e5186ed2a609eba8f72fede285ac06ffd1055
jkroh1995 <shworud29@gmail.com>
2023-07-24 13:45:47 +0900

    refactor : 불필요한 join 제거

--- server/src/main/java/project/server/domain/recommend/repository/RecommendRepository.java
+++ server/src/main/java/project/server/domain/recommend/repository/RecommendRepository.java
@@ -4,26 +4,24 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import project.server.domain.cocktail.entity.Cocktail;
+import project.server.domain.bookmark.entity.Bookmark;
 import project.server.domain.recommend.entity.Recommend;
 
 import java.util.List;
 
-public interface RecommendRepository extends JpaRepository<Cocktail, Long> {
+public interface RecommendRepository extends JpaRepository<Bookmark, Long> {
 
-    @Query(value = "select distinct b.cocktail_id as cocktailId, cocktail_name as name, cocktail_image_url as imageUrl " +
-            "from cocktails c " +
-            "right join bookmarks b on b.cocktail_id = c.cocktail_id " +
-            "group by b.cocktail_id, b.gender, b.age " +
-            "order by COUNT(b.cocktail_id) desc", nativeQuery = true)
+    @Query(value = "select distinct cocktail_id as cocktailId, cocktail_name as name, cocktail_image_url as imageUrl " +
+            "from bookmarks b " +
+            "group by cocktail_id, gender, age " +
+            "order by COUNT(cocktail_id) desc", nativeQuery = true)
     List<Recommend> findBestCocktails(Pageable pageable);
 
-    @Query(value = "select distinct b.cocktail_id as cocktailId, cocktail_name as name, cocktail_image_url as imageUrl " +
-            "from cocktails c " +
-            "right join bookmarks b on b.cocktail_id = c.cocktail_id " +
-            "where b.age=:age and b.gender=:gender " +
-            "group by b.cocktail_id, b.gender, b.age " +
-            "order by COUNT(b.cocktail_id) desc ", nativeQuery = true)
+    @Query(value = "select distinct cocktail_id as cocktailId, cocktail_name as name, cocktail_image_url as imageUrl " +
+            "from bookmarks b " +
+            "where age=:age and gender=:gender " +
+            "group by cocktail_id, gender, age " +
+            "order by COUNT(cocktail_id) desc ", nativeQuery = true)
     List<Recommend> findRecommendCocktails(@Param(value = "age") int userAgeGroup,
                                            @Param(value = "gender") String gender,
                                            Pageable pageable);
