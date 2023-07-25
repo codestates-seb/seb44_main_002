9b2a96fc358db3c0b6ed354ea7dfe93442f5c17d
crowcrow07 <skathd3402@gmail.com>
2023-07-25 19:52:35 +0900

    fixed : 슬라이더 시간 변경

--- client/src/pages/Main/Slider/Slider.js
+++ client/src/pages/Main/Slider/Slider.js
@@ -76,7 +76,7 @@ export default function Slider() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         pagination={{
