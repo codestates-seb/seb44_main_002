7bbb4112948bb6ad6f270df14fb05a2f17f3e65a
jkroh1995 <shworud29@gmail.com>
2023-07-26 15:55:04 +0900

    fix : 비로그인 랭킹 중복 오류 수정

--- server/src/main/java/project/server/domain/recommend/repository/RecommendRepository.java
+++ server/src/main/java/project/server/domain/recommend/repository/RecommendRepository.java
@@ -14,7 +14,7 @@ public interface RecommendRepository extends JpaRepository<Bookmark, Long> {
     @Query(value = "select distinct c.cocktail_id as cocktailId, c.name as name, c.image_url as imageUrl, count(c.cocktail_id) as cocktail_count " +
             "from cocktails c " +
             "right join bookmarks b on b.cocktail_cocktail_id = c.cocktail_id " +
-            "group by cocktail_id, gender, age " +
+            "group by cocktail_id " +
             "order by cocktail_count desc", nativeQuery = true)
     List<Recommend> findBestCocktails(Pageable pageable);
 
