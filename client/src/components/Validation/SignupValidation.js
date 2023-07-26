096b9bc6725bb138e7d57a895f3ec76925742c77
crowcrow07 <skathd3402@gmail.com>
2023-07-26 21:18:58 +0900

    fixed : 서버 유효성검사 미적용으로 인한 롤백

--- client/src/components/Validation/SignupValidation.js
+++ client/src/components/Validation/SignupValidation.js
@@ -2,8 +2,9 @@ export default function UseSignupValid(form) {
   const nameRegex = /^(?!.*편한$)[^\s]{2,9}$/;
   const emailRegex =
     /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  const passwordRegex =
-    /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{8,}$/;
+  // const passwordRegex =
+  //   /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{8,}$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
   const ageRegex = /^(?:20|[2-9][0-9]|1[0-1][0-9]|12[0-2])$/;
 
   const nameIsValid = form.name && nameRegex.test(form.name);
