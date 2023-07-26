52c0ab5c211f53f6ee7cd4f8f03684f686d097f8
jkroh1995 <shworud29@gmail.com>
2023-07-26 12:08:48 +0900

    fix : 북마크 - 칵테일 연관관계 설정 및 랭킹 기능 수정

--- server/src/main/java/project/server/global/config/AppConfiguration.java
+++ server/src/main/java/project/server/global/config/AppConfiguration.java
@@ -1,44 +1,39 @@
-//package project.server.global.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.InitializingBean;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//import project.server.domain.cocktail.embed.category.Category;
-//import project.server.domain.cocktail.embed.ingredient.Ingredients;
-//import project.server.domain.cocktail.embed.liquor.Liquor;
-//import project.server.domain.cocktail.embed.rate.Rate;
-//import project.server.domain.cocktail.embed.recipe.Recipe;
-//import project.server.domain.cocktail.embed.tag.Tag;
-//import project.server.domain.cocktail.embed.tag.Tags;
-//import project.server.domain.cocktail.entity.Cocktail;
-//import project.server.domain.cocktail.repository.CocktailRepository;
-//import project.server.domain.user.entity.User;
-//import project.server.domain.user.repository.UserRepository;
-//
-//import java.util.ArrayList;
-//import java.util.LinkedHashSet;
-//import java.util.List;
-//import java.util.Set;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class AppConfiguration implements InitializingBean {
-//
-//    private final UserRepository userRepository;
-//    private final CocktailRepository cocktailRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Override
-//    public void afterPropertiesSet() throws Exception {
-//        init();
-//    }
-//
-//    private User selectRandomUser(List<User> users) {
-//        int index = (int) (Math.random()*users.size());
-//        if(index == users.size()){
-//            index--;
-//        }
-//        return users.get(index);
-//    }
-//}
+package project.server.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import project.server.domain.bookmark.repository.BookmarkRepository;
+import project.server.domain.cocktail.repository.CocktailRepository;
+import project.server.domain.user.entity.User;
+import project.server.domain.user.repository.UserRepository;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AppConfiguration implements InitializingBean {
+
+    private final UserRepository userRepository;
+    private final CocktailRepository cocktailRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        init();
+    }
+
+    private void init() {
+        bookmarkRepository.deleteAll();
+    }
+
+    private User selectRandomUser(List<User> users) {
+        int index = (int) (Math.random()*users.size());
+        if(index == users.size()){
+            index--;
+        }
+        return users.get(index);
+    }
+}
