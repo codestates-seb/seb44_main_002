9138ef6d24af1a06777ee2f762c8b2f6a4885b69
park-tae-yang <pxodid2000@gmail.com>
2023-07-26 15:03:51 +0900

    fix : 비밀번호 유효성 검증 애노테이션 수정 매치 패턴을 생성해 회원가입마다 GC가 생기는걸 줄였습니다

--- server/src/main/java/project/server/global/validator/PasswordValidator.java
+++ server/src/main/java/project/server/global/validator/PasswordValidator.java
@@ -4,14 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class PasswordValidator implements ConstraintValidator<Password, String> {
     private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$";
-
-    @Override
-    public void initialize(Password constraintAnnotation) {
-    }
+    private static final int PASSWORD_LENGTH = 8;
+    private static final Pattern PASSWORD_NUMBER = Pattern.compile(".*[0-9].*");
+    private static final Pattern PASSWORD_SPECIAL_CHAR = Pattern.compile(".*[!@#$%^&*].*");
+    private static final Pattern PASSWORD_CHARACTER = Pattern.compile(".*[a-zA-Z].*");
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
@@ -23,19 +24,23 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
 
         if (!isValid) {
             log.info("# PasswordValidator : 비밀번호 유효성 검사 실패");
-            if (password.length() < 8) {
+            if (password.length() < PASSWORD_LENGTH) {
                 log.info("비밀번호는 8글자 이상이어야 합니다.");
             }
-            if (!password.matches(".*[0-9].*")) {
+            if (!PASSWORD_NUMBER.matcher(password).matches()) {
                 log.info("비밀번호는 숫자를 포함해야 합니다.");
             }
-            if (!password.matches(".*[!@#$%^&*].*")) {
+            if (!PASSWORD_SPECIAL_CHAR.matcher(password).matches()) {
                 log.info("비밀번호는 특수문자를 포함해야 합니다.");
             }
-            if (!password.matches(".*[a-zA-Z].*")) {
+            if (!PASSWORD_CHARACTER.matcher(password).matches()) {
                 log.info("비밀번호는 문자를 포함해야 합니다.");
             }
         }
         return isValid;
+    }
+
+    @Override
+    public void initialize(Password constraintAnnotation) {
     }
 }
