0ef4621aa0c3ab93b448711c5ef6f5ea5f34b3d0
crowcrow07 <skathd3402@gmail.com>
2023-07-26 14:28:06 +0900

    fixed : 비밀번호 유효성 검사 에러 메시지 다양하게 뜨게함

--- client/src/components/Validation/SignupValidation.js
+++ client/src/components/Validation/SignupValidation.js
@@ -2,8 +2,9 @@ export default function UseSignupValid(form) {
   const nameRegex = /^(?!.*편한$)[^\s]{2,9}$/;
   const emailRegex =
     /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-  const ageRegex = /^(?:1[0-1][0-9]|12[0-2]|1[0-9]|[2-9][0-9])$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{8,}$/;
+  const ageRegex = /^(?:20|[2-9][0-9]|1[0-1][0-9]|12[0-2])$/;
 
   const nameIsValid = form.name && nameRegex.test(form.name);
   const emailIsValid = form.email && emailRegex.test(form.email);
