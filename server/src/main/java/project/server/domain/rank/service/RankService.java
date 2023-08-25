f876827f3584310bbd6e9841f8299b4a73129f3a
jkroh1995 <shworud29@gmail.com>
2023-08-25 11:02:24 +0900

    refactor : 불필요하게 분리되어있던 객체 통합

--- server/src/main/java/project/server/domain/rank/service/RankService.java
+++ server/src/main/java/project/server/domain/rank/service/RankService.java
@@ -1,8 +1,11 @@
 package project.server.domain.rank.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import project.server.domain.rank.dto.RankDto;
+import project.server.domain.rank.repository.RankRepository;
 import project.server.domain.rank.vo.Rank;
 import project.server.domain.user.entity.User;
 import project.server.domain.user.service.UserService;
@@ -13,26 +16,33 @@ import java.util.List;
 @Slf4j
 public class RankService {
 
-    private final RankReadService rankReadService;
+    private static final int DEFAULT_ELEMENTS_COUNT = 5;
+    private static final int DEFAULT_PAGE_NUMBER = 0;
+    private static final Pageable pageable = PageRequest.ofSize(DEFAULT_ELEMENTS_COUNT).withPage(DEFAULT_PAGE_NUMBER);
+
+    private final RankRepository rankRepository;
     private final UserService userService;
 
-    public RankService(RankReadService rankReadService, UserService userService) {
-        this.rankReadService = rankReadService;
+    public RankService(RankRepository rankRepository, UserService userService) {
+        this.rankRepository = rankRepository;
         this.userService = userService;
     }
 
-
     public RankDto.UnsignedResponse readRankForUnsignedUser() {
-        List<Rank> totalRank =  rankReadService.readTotalRank();
+        List<Rank> totalRank =  rankRepository.findBestCocktails(pageable);
         log.info("# RankService#readRankForUnsignedUser 성공");
         return new RankDto.UnsignedResponse(totalRank);
     }
 
     public RankDto.SignedResponse readRankForSignedUser(String email) {
         User user = userService.findUserByEmail(email);
-        List<Rank> totalRank = rankReadService.readTotalRank();
-        List<Rank> signedUserRank = rankReadService.readSignedUserRank(user);
+        List<Rank> totalRank = rankRepository.findBestCocktails(pageable);
+        List<Rank> signedUserRank = rankRepository.findRecommendCocktails(getUserAgeGroup(user), user.getGender(), pageable);
         log.info("# userId : {}, userAge : {}, userGender : {} RankService#readRankForSignedUser 성공", user.getUserId(), user.getAge(), user.getGender());
         return new RankDto.SignedResponse(totalRank, signedUserRank);
+    }
+
+    private int getUserAgeGroup(User user) {
+        return user.getAge() / 10 * 10;
     }
 }
